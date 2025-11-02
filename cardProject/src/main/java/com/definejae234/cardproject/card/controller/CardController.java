8cd3f08f6d0b825533ffe29b71ac8560fdd0daec
jaehyeonjeong <definejae234@gmail.com>
2025-11-02 22:29:04 +0900

    프로젝트 : 카드 추천 프로젝트 수정인 : 정재현 수정코드 : project_card_20251102_2 수정날짜 : 2025/11/02 수정내용 :  1. 카드 추천 프로젝트 뼈대 제작 진행중  - 카드 기본 정보 리스트, 정보 수정 기능 추가 ※명일 카드의 결제 브랜드,  카드 혜택 정보를 Merge로 데이터를 저장해보도록 시도 1) 데이터베이스에서 먼저 테스트 2) Mapper 적용 테스트

--- cardProject/src/main/java/com/definejae234/cardproject/card/controller/CardController.java
+++ cardProject/src/main/java/com/definejae234/cardproject/card/controller/CardController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.smartcardio.Card;
+import java.util.List;
 
 @Controller
 public class CardController {
@@ -49,5 +50,36 @@ public class CardController {
             return "redirect:/card/home";
         }
         return "card/insert";
+    }
+
+    @GetMapping("/card/list")
+    public String list(Model model){
+        List<CardDto> cardDtoList = cardDao.cardListInfo();
+        model.addAttribute("cardDtoList",cardDtoList);
+        return "card/list";
+    }
+
+    @GetMapping("/card/{id}/info")
+    public String cardInfo(@PathVariable("id") int id, Model model){
+        CardDto cardInfoDto = cardDao.cardFindById(id);
+        model.addAttribute("cardInfoDto",cardInfoDto);
+        return "card/info";
+    }
+
+    @PostMapping("/card/{id}/info")
+    public String cardInfoProcess(@PathVariable("id") int id,
+                                  @ModelAttribute("cardInfoDto") CardDto cardDto,
+                                  Model model){
+
+        cardDto.setId(id); // 안전하게 ID 설정
+        int result = cardDao.cardUpdateInfo(cardDto);
+
+        if (result > 0) {
+            return "redirect:/card/list";
+        }
+
+        model.addAttribute("cardInfoDto", cardDto);
+        return "card/info";
+
     }
 }
--- cardProject/src/main/java/com/definejae234/cardproject/card/dao/CardDao.java
+++ cardProject/src/main/java/com/definejae234/cardproject/card/dao/CardDao.java
@@ -3,7 +3,12 @@ package com.definejae234.cardproject.card.dao;
 import com.definejae234.cardproject.card.dto.CardDto;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface CardDao {
     int cardInsertInfo(CardDto cardDto);
+    List<CardDto> cardListInfo();
+    CardDto cardFindById(int id);
+    int cardUpdateInfo(CardDto cardDto);
 }
