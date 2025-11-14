ccfbde60faa82764864b099c4f794f2dcb74e587
tjdwn4276 <ksj000915@gmail.com>
2025-11-14 09:31:36 +0900

    --secondPage 카드사,브랜드,실적,연회비 선택에 따라 카드 리스트 조회

--- cardProject/src/main/java/com/definejae234/cardproject/card/CardBenefitEnum.java
+++ cardProject/src/main/java/com/definejae234/cardproject/card/CardBenefitEnum.java
@@ -1,4 +1,17 @@
 package com.definejae234.cardproject.card;
 
+import lombok.Getter;
+
+@Getter
 public enum CardBenefitEnum {
+    fuel("주유"),
+    comm("통신"),
+    shop("쇼핑"),
+    food("음식"),
+    cafe("카페");
+
+    private final String name;
+    CardBenefitEnum(String name) {
+        this.name = name;
+    }
 }
