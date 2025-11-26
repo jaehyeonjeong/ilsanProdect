879e430357729718e832ab15d090963d4c7552a1
jjyy0614 <jjyy0614@naver.com>
2025-11-26 10:29:37 +0900

    프로젝트 : 카드 추천 프로젝트 수정인 : 김성주 수정코드 : project_card_20251126 수정날짜 : 2025/11/26 수정내용 :  - 정보수정 본인 이메일과 핸드폰 사용시 수정안되는 사항 수정  - Vaild까지 추가하였음

--- cardProject/src/main/java/com/definejae234/cardproject/member/dto/EditDto.java
+++ cardProject/src/main/java/com/definejae234/cardproject/member/dto/EditDto.java
@@ -1,5 +1,7 @@
 package com.definejae234.cardproject.member.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,8 +11,12 @@ import org.springframework.web.multipart.MultipartFile;
 @AllArgsConstructor
 @Builder
 public class EditDto {
+    @NotBlank(message = "이름은 필수 입력사항입니다")
     private String userName;
+    @NotBlank(message = "이메일은 필수 입력사항입니다.")
+    @Email(message = "이메일 형식에 맞게 입력해주세요.")
     private String userEmail;
+    @NotBlank(message = "전화번호는 필수 입력사항입니다.")
     private String phone;
     private String zipcode;
     private String address01;
